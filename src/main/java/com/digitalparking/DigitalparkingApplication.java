368f2fbab544a1ff0ac021ac93f81f42195d4ee7
fred <fredrischter@gmail.com>
2018-11-04 10:38:11 -0300

    Moving the application main classe for the root package.

--- src/main/java/com/digitalparking/DigitalparkingApplication.java
+++ src/main/java/com/digitalparking/DigitalparkingApplication.java
@@ -1,4 +1,4 @@
-package com.digitalparking.digitalparking;
+package com.digitalparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
